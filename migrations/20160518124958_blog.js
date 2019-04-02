6201d55f5dcfa78f0e5f96d584a868da3a5334c5
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-02 00:33:50 +0000

    Automatically backed up by Learn

--- migrations/20160518124958_blog.js
+++ migrations/20160518124958_blog.js
@@ -5,16 +5,16 @@ exports.up = function(knex, Promise) {
     tbl.string('email');
     tbl.string('username');
     tbl.string('name');
-  }).createTable('posts', table => {
-    table.increments('id').primary();
-    table.string('body');
-    table.string('title');
-    table.integer('user_id').references('users.id');
-  }).createTable('comments', table => {
-    table.increments('id').primary();
-    table.string('post_id').references('posts.id');
-    table.string('user_id').references('users.id');
-    table.string('body');
+  }).createTable('posts', tbl => {
+    tbl.increments('id').primary();
+    tbl.string('body');
+    tbl.string('title');
+    tbl.integer('user_id').references('users.id');
+  }).createTable('comments', tbl => {
+    tbl.increments('id').primary();
+    tbl.string('post_id').references('posts.id');
+    tbl.string('user_id').references('users.id');
+    tbl.string('body');
   });
 };
 
