f1c4cf588d22e50110abe371a03adee825df72d8
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-02 00:26:02 +0000

    Automatically backed up by Learn

--- migrations/20160518124958_blog.js
+++ migrations/20160518124958_blog.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  knex.schema.createTable('users').then((tbl) => {
+    tbl.increments('id').primary();
+    tbl.string('email');
+    tbl.string('username');
+    tbl.string('name');
+    tbl.timestamps() ;
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  knex.schema.destroyTable('users').destroyTable('posts').destroyTable('comments')
 };
